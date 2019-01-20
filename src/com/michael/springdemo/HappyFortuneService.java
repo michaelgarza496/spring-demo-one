f69581be89df4adb5c66295230c0305cffe32ffc
Michael Garza <michaelgarza496@gmail.com>
2019-01-20 17:11:45 -0500

    sport.properties is used

--- src/com/michael/springdemo/HappyFortuneService.java
+++ src/com/michael/springdemo/HappyFortuneService.java
@@ -1,10 +1,20 @@
 package com.michael.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
+	private static String fortunes[] = new String[3];
+	
+	public HappyFortuneService() {
+		fortunes[0] = "Today is your very lucky day";
+		fortunes[1] = "You have to poop!";
+		fortunes[2] = "ohh... you know...";
+	}
+	
 	@Override
 	public String getDailyFortune() {
-		return "Today is your lucky day";
+		return fortunes[new Random().nextInt(3)];
 	}
 
 }
