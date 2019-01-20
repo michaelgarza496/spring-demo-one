3c5f22ddea517bca53e4a426bf5ebc92a715cf23
Michael Garza <michaelgarza496@gmail.com>
2019-01-20 13:10:44 -0500

    Literal values injection

--- src/com/michael/springdemo/CricketCoach.java
+++ src/com/michael/springdemo/CricketCoach.java
@@ -4,9 +4,31 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// add new fields for email address and team
+	private String emailAddress;
+	private String team;
+
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg contructor");
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
 	}
 	
 	public void setFortuneService(FortuneService fortuneService) {
